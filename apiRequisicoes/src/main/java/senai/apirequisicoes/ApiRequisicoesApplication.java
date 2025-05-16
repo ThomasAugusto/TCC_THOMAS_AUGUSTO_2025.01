08d744f6386bdbb1d5f2102af1012bbdc52fe46d
Thomas Villeroy <thomasvilleroy@outlook.com>
2025-05-16 11:28:56 -0300

    PrimeiroCommit

---
+++ apiRequisicoes/src/main/java/senai/apirequisicoes/ApiRequisicoesApplication.java
@@ -0,0 +1,13 @@
+package senai.apirequisicoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRequisicoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRequisicoesApplication.class, args);
+    }
+
+}
---
+++ apiRequisicoes/src/test/java/senai/apirequisicoes/ApiRequisicoesApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.apirequisicoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRequisicoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
