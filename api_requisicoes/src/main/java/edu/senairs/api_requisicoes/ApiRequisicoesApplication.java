9ab17d042583257794c4b88410e1bb136f345bf6
Thomas Villeroy <thomasvilleroy@outlook.com>
2025-05-21 15:43:56 -0300

    Firt-Commit

---
+++ api_requisicoes/src/main/java/edu/senairs/api_requisicoes/ApiRequisicoesApplication.java
@@ -0,0 +1,13 @@
+package edu.senairs.api_requisicoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRequisicoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRequisicoesApplication.class, args);
+	}
+
+}
---
+++ api_requisicoes/src/test/java/edu/senairs/api_requisicoes/ApiRequisicoesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.senairs.api_requisicoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRequisicoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
